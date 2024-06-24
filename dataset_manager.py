ff6e8b3dd620a0491516caa14fb93a4977f14cae
ardakacar <arda.kacar21@gmail.com>
2024-06-24 13:14:24 +0200

    Update dataset_manager.py
    Changed the entire logic in get_recipe_recommendations function.

--- dataset_manager.py
+++ dataset_manager.py
@@ -16,11 +16,23 @@ def get_recipe_recommendations(ingredients, exclude_ingredients, cuisine, max_to
     #return "1. boil pasta with sauce then add oil in pan add sauce cook sauce for 5 minutes and boiled pasta enjoy"
 
     matching_dishes = df[df['cuisine_path'].str.contains(cuisine, case=False)]
-    matching_dishes = matching_dishes[matching_dishes['ingredients'].apply(lambda x: set(ingredients).issubset(set(literal_eval(x))))]
-
+    #matching_dishes = matching_dishes[matching_dishes['ingredients'].apply(lambda x: set(ingredients).issubset(set(literal_eval(x))))]
+    
+    def has_required_ingredients(dish_ingredients):
+        dish_ingredients_set = set(literal_eval(dish_ingredients))
+        return set(ingredients).issubset(dish_ingredients_set)
+    
+    matching_dishes = matching_dishes[matching_dishes['ingredients'].apply(has_required_ingredients)]
+    
     if exclude_ingredients:
-        for exclude_ingredient in exclude_ingredients:
-            matching_dishes = matching_dishes[~matching_dishes['ingredients'].apply(lambda x: exclude_ingredient in literal_eval(x))]
+        def exclude_undesired_ingredients(dish_ingredients):
+            dish_ingredients_set = set(literal_eval(dish_ingredients))
+        
+            return not any(exclude_ingredient in dish_ingredients_set for exclude_ingredient in exclude_ingredients)
+        #for exclude_ingredient in exclude_ingredients:
+        #    matching_dishes = matching_dishes[~matching_dishes['ingredients'].apply(lambda x: exclude_ingredient in literal_eval(x))]
+        
+        matching_dishes = matching_dishes[matching_dishes['ingredients'].apply(exclude_undesired_ingredients)]
     
     if max_total_time is not None:
         matching_dishes = matching_dishes[matching_dishes['total_time'] <= max_total_time]
